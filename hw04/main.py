276549bcd0c5b29d643980dc128f69add79ce152
Skylar1210 <37677868+Skylar1210@users.noreply.github.com>
2018-10-24 00:53:49 +0800

    Update main.py

--- hw04/main.py
+++ hw04/main.py
@@ -1,15 +1,16 @@
 class shapes(object):
-    def __init__(self,width,height):
-        self.width = width
-        self.height = height
+
+    @property
     def area(self):
         pass
-    sum=0
 
+    
 class rectangle(shapes):
-    def __init__(self,width,height):
+    def __init__(self,width=0,height=0):
+        super().__init__()
         self.width = width
         self.height = height
+    @property
     def area(self):
         return self.width*self.height
 
@@ -17,23 +18,30 @@ class rectangle(shapes):
 
 class square(rectangle):
 
-    def __init__(self,width):
+    def __init__(self,width=0):
+        super().__init__()
         self.width=width
         self.height=width
 
 class ellipse(rectangle):
 
-    def __init__(self,width,length):
+    def __init__(self,width=0,height=0):
+        super().__init__()
         self.width=width
-        self.height=length*3.14
+        self.height=height*3.14
 
 class circle(rectangle):
 
-    def __init__(self,width):
+    def __init__(self,width=0):
+        super().__init__()
         self.width=width
         self.height=width*3.14
+
+
         
         
 shapes = [ellipse(10, 20), square(15), circle(5),rectangle(20, 15), circle(5), square(15),ellipse(10, 20)]        
 compute_area=[x.area for x in shapes]
+print（compute_area）
 total_area=sum(compute_area)
+print(total_area)
